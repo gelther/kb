c333eafa70637f086fba0d0453799ce7966995a9
JeroenSormani <jeroen.sormani@gmail.com>
2016-05-14 16:04:24 +0200

    Code consistency changes for includes/class-utils.php

--- includes/class-utils.php
+++ includes/class-utils.php
@@ -9,22 +9,19 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 
 class Kanban_Utils
 {
-	static function get_nonce()
-	{
+	static function get_nonce() {
 		return sprintf( '%s_nonce', Kanban::get_instance()->settings->basename );
 	}
 
 
 
-	static function build_array_with_id_keys( $arr, $id_key = 'id' )
-	{
+	static function build_array_with_id_keys( $arr, $id_key = 'id' ) {
 		$return = array();
 
-		foreach ( $arr as $obj )
-		{
+		foreach ( $arr as $obj ) {
 			if ( is_array( $obj ) ) $obj = (object) $obj;
 
-			$return[$obj->$id_key] = $obj;
+			$return[ $obj->$id_key ] = $obj;
 		}
 
 		return $return;
@@ -32,35 +29,29 @@ class Kanban_Utils
 
 
 
-	static function make_word_plural( $word )
-	{
+	static function make_word_plural( $word ) {
 		return mb_substr( $word, -1 ) == 's' ? sprintf( __( '%ses', 'kanban' ), $word ) : sprintf( __( '%ss', 'kanban' ), $word );
 	}
 
 
 
 	// http://stackoverflow.com/a/17694792/38241
-	static function find_key_of_object_by_property( $prop, $val, $arr )
-	{
-		foreach ( $arr as $key => $item )
-		{
-			if ( $val == $item->$prop )
-			{
+	static function find_key_of_object_by_property( $prop, $val, $arr ) {
+		foreach ( $arr as $key => $item ) {
+			if ( $val == $item->$prop ) {
 				return $key;
 				break;
 			}
 		}
 
-		return FALSE;
+		return false;
 	}
 
 
 
-	static function order_array_of_objects_by_property( $arr, $property, $cmp_type = 'str' )
-	{
+	static function order_array_of_objects_by_property( $arr, $property, $cmp_type = 'str' ) {
 
-		if ( $cmp_type == 'int' )
-		{
+		if ( $cmp_type == 'int' ) {
 			usort(
 				$arr,
 				function( $a, $b ) use ( $property )
@@ -71,9 +62,7 @@ class Kanban_Utils
 					return ( (int) $a->$property < (int) $b->$property ) ? -1 : 1;
 				}
 			);
-		}
-		else
-		{
+		} else {
 			usort(
 				$arr,
 				function( $a, $b ) use ( $property )
@@ -91,15 +80,13 @@ class Kanban_Utils
 
 
 
-	static function mysql_now_gmt()
-	{
+	static function mysql_now_gmt() {
 		return gmdate( 'Y-m-d H:i:s' );
 	}
 
 
 
-	static function str_for_frontend( $str )
-	{
+	static function str_for_frontend( $str ) {
 			return htmlentities( stripcslashes( stripcslashes( stripcslashes( $str ) ) ), ENT_QUOTES );
 	}
 }
